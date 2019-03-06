7b212f2f890d315d52ccded6a0e5611aada07570
github username <github email address>
2019-03-06 17:43:36 +0000

    Done.

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,13 +1,12 @@
-def FizzBuzz(int)
-  if int % 3==0
+def fizzbuzz(int)
+  if int % 3==0 && int % 5==0
+      "FizzBuzz"
+  elsif int % 3==0
     "Fizz"
-  end
-  if int % 5==0
+
+  elsif int % 5==0
     "Buzz"
+  else nil
   end
-  if (int % 3==0)&&(int % 5==0)
-    "FizzBuzz"
-  end
- 
 end # Don't forget! This file needs to be 'required' in its spec file
 # See README.md for instructions on how to do this
